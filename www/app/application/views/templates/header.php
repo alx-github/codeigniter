be4baf4dc9c174294c189a1d242ef47537b26fc5
ALLEXCEED\lamtd <lamtd@allexceed.vn>
2017-07-05 11:47:18 +0700

    login completed

---
+++ www/app/application/views/templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>title</title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/umi/css/bootstrap.min.css') ?> ">
+</head>
+<body>
