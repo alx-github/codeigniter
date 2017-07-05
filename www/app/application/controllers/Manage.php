eb6c7c3f7b2ad9c2cc9759574c58df35fd5c3b87
ALLEXCEED\lamtd <lamtd@allexceed.vn>
2017-07-05 13:17:34 +0700

    login completed

---
+++ www/app/application/controllers/Manage.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Manage extends MY_Controller {
+	
+	public function index()
+	{
+		echo "manage";
+		// $this->load->view('templates/header');
+		// $this->load->view('templates/welcome_message');
+		// $this->load->view('templates/footer');
+	}
+	
+}
